a39b56de45395ec030cecda382b207de43bdeec0
caleb <calebpirkle2@gmail.com>
2025-11-17 00:22:35 -0600

    league endpoint

--- src/api/league.ts
+++ src/api/league.ts
@@ -4,16 +4,9 @@ import { Sleeper } from '../lib/sleeper.js';
 export async function handlerLeague(_: Request, res: Response) {
 	const sleeper = new Sleeper();
 	const league = await sleeper.getLeague();
-	const rosters = await sleeper.getLeagueRosters();
-	const users = await sleeper.getLeagueUsers();
-	const playoff = await sleeper.getLeaguePlayoffBracket('losers_bracket');
-	const matchups = await sleeper.getThisWeeksLeagueMatchups(1);
-	const players = await sleeper.getAllPlayers();
 	const data = {
-		players
+		league
 	};
-
-
 
 	res.send(data);
 }
