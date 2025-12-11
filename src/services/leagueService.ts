a6dc4f0135c3afe130fa9fe8af67d8d8d05f5288
caleb <calebpirkle2@gmail.com>
2025-12-11 11:20:48 -0600

    removed comments

--- src/services/leagueService.ts
+++ src/services/leagueService.ts
@@ -4,29 +4,29 @@ import { Sleeper } from "../lib/sleeper.js";
 import { undefinedToNullDeep, normalizeString } from "../lib/helpers.js";
 
 export async function buildLeagueHistory() {
-    // const sleeper = new Sleeper();
-    // const rawCurrentLeague = await sleeper.getLeague();
-    // const rawPreviousLeagues = await getPreviousLeagues(rawCurrentLeague);
-    // const rawAllLeagues = [rawCurrentLeague, ...(rawPreviousLeagues ?? [])];
-    // const strictAllLeagues = undefinedToNullDeep(rawAllLeagues) as unknown as Required<Array<InsertLeague>>;
+    const sleeper = new Sleeper();
+    const rawCurrentLeague = await sleeper.getLeague();
+    const rawPreviousLeagues = await getPreviousLeagues(rawCurrentLeague);
+    const rawAllLeagues = [rawCurrentLeague, ...(rawPreviousLeagues ?? [])];
+    const strictAllLeagues = undefinedToNullDeep(rawAllLeagues);
 
-    // return strictAllLeagues.map(league => {
-    //     const statusString = normalizeString(league.status);
-    //     const leagueStatus = statusString === 'in_season' || statusString === "post_season" ? true : false;
-    //     const rosterPositions = league.roster_positions.map(position => normalizeString(position));
+    return strictAllLeagues.map(league => {
+        const statusString = normalizeString(league.status);
+        const leagueStatus = statusString === 'in_season' || statusString === "post_season" ? true : false;
+        const rosterPositions = league.roster_positions.map(position => normalizeString(position));
 
-    //     return {
-    //         leagueId: normalizeString(league.league_id),
-    //         season: normalizeString(league.season),
-    //         status: leagueStatus,
-    //         leagueName: normalizeString(league.name),
-    //         draftId: normalizeString(league.draft_id),
-    //         totalRosters: league.total_rosters,
-    //         rosterPositions: rosterPositions,
-    //         avatarId: normalizeString(league.avatar),
-    //         previousLeagueId: league.previous_league_id ? normalizeString(league.previous_league_id) : null,
-    //     } satisfies InsertLeague;
-    // });
+        return {
+            leagueId: normalizeString(league.league_id),
+            season: normalizeString(league.season),
+            status: leagueStatus,
+            leagueName: normalizeString(league.name),
+            draftId: normalizeString(league.draft_id),
+            totalRosters: league.total_rosters,
+            rosterPositions: rosterPositions,
+            avatarId: normalizeString(league.avatar),
+            previousLeagueId: league.previous_league_id ? normalizeString(league.previous_league_id) : null,
+        } satisfies InsertLeague;
+    });
 }
 
 export async function getPreviousLeagues(league: RawLeague): Promise<RawLeague[] | null> {
