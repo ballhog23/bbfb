b31b75fafdf1871b804f7f16c96fb9ffd5d47f5c
caleb <calebpirkle2@gmail.com>
2026-01-19 03:24:12 -0600

    fuck

--- src/web/index.ts
+++
@@ -1,8 +0,0 @@
-import type { Request, Response } from "express";
-import { selectLeagueWinner } from "../db/queries/playoffs.js";
-import { config } from "../config.js";
-
-export async function handlerRenderIndex(_: Request, res: Response) {
-    const recentWinner = await selectLeagueWinner(config.league.id);
-    res.render('pages/index', { recentWinner });
-}
